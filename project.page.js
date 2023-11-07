d3384ca55931a01990662e181c6ae19f818fbc32
Tatiana Barga <tatibspersonal@gmail.com>
2023-11-06 21:27:00 -0600

    created and started project page file and manually made changes to html doc to matc h Max's updates because I couldnt pull them pt2

--- project.page.js
+++ project.page.js
@@ -1,2 +1,30 @@
 const app1 = document.querySelector('#app1');
 const app2 = document.querySelector('#app2');
+
+const displayForm = () => {
+ const form = 
+ `<div class="mb-3">
+ <label for="exampleFormControlInput1" class="form-label">Email address</label>
+ <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+ <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+ <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>`;
+app2 = form;
+}
+
+const projectList = 
+`<div class="container">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>`
