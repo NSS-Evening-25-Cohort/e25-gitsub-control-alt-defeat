83b0534ddf104c39b6c5fa1a4d9445f71f55f3a6
MaxRubel <maxrubel1@gmail.com>
2023-11-07 20:25:37 -0600

    about to reformat to javascript

---
+++ main.js
@@ -0,0 +1,25 @@
+let user = {
+  userPhoto: `"("./images/tomJones.jpg")"`,
+  name: "Tom Jones",
+  userId: "TommyJ",
+  tagLine:
+    "Doing big things and being awesome. Seriously the greatest. Built different.",
+  stats: "10mil Followers - 27 following -500",
+  location: "London, Englad",
+  email: "github@tomJones.com",
+  website: "https://www.tomjones.com",
+  twitter: "@TommyJoJo",
+  highlights: `"Artict Code Vault Contributor <br>
+  Git Hub Star <br> PRO"`,
+  organizations: "",
+  sponsors: "",
+};
+
+const userPhoto = document.querySelector("userPhoto");
+const realName = document.querySelector("realName");
+const userName = document.querySelector("userName");
+const tagLine = document.querySelector("tagLine");
+const stats = document.querySelector("stats");
+const personalInfo = document.querySelector("personalInfo");
+const highlights = document.querySelector("highlights");
+const sponsors = document.querySelector("sponsors");
