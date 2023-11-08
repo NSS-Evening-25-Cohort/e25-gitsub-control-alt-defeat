264b894f9bcf86e12fc594e1005523f7cb1fa198
Jesse Ramirez <gideonramirezz@gmail.com>
2023-11-07 20:09:42 -0600

    MVP for group project for repo page reached

---
+++ .history/repoPage_20231107181803.js
@@ -0,0 +1,95 @@
+const cards = [
+  {
+    id: 1,
+    name: "testero",
+    description: "string",
+    keys: "netlify",
+    language: "javascript",
+    favorite: "yes",
+    forks: 12,
+    issues: 2,
+    updates: 9
+  },
+  {
+    id: 2,
+    name: "testero",
+    description: "string",
+    keys: "netlify",
+    language: "javascript",
+    favorite: "yes",
+    forks: 12,
+    issues: 2,
+    updates: 9
+  },
+  {
+    id: 3,
+    name: "testero",
+    description: "string",
+    keys: "netlify",
+    language: "javascript",
+    favorite: "yes",
+    forks: 12,
+    issues: 2,
+    updates: 9
+  },
+  {
+    id: 4,
+    name: "testero",
+    description: "string",
+    keys: "netlify",
+    language: "javascript",
+    favorite: "yes",
+    forks: 12,
+    issues: 2,
+    updates: 9
+  },
+];
+
+const renderToDom = (divId, textToRender) => {
+  const selectedElement = document.querySelector(divId);
+  selectedElement.innerHTML = textToRender; 
+}
+
+const renderRepo = (array) => {
+  let domString = "";
+  array.forEach((card) => {
+    console.log()
+    domString += `<div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${card.name}</h5>
+        <p class="card-text">${card.description}</p>
+      </div>
+    </div>`;
+  });
+  renderToDom('#app2', domString);
+}
+
+const createRepo = (e) => {
+  e.preventDefault();
+  const newRepo = {
+    id: cards.length + 1,
+    name: document.getElementById('repo-name').value,
+    description: document.getElementById('description').value,
+  }
+  cards.push(newRepo);
+  renderRepo(cards);
+}
+
+const newRepoForm = () => {
+  const domString = `<form id="repoForm" class="mb-3">
+    <label for="repo-name" class="form-label">Repository Name</label>
+    <input type="text" class="form-control" id="repo-name" >
+    <label for="description" class="form-label">Description</label>
+    <input type="text" class="form-control" id="description" >
+    <button type="submit" class="btn btn-primary">Create Repo</button>
+  </form>`;
+  renderToDom('#app1', domString);
+  document.querySelector('#repoForm').addEventListener('submit', createRepo);
+}
+
+const startApp = () => {
+  renderRepo(cards);
+  newRepoForm();
+}
+
+startApp();
