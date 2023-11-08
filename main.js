b5deb03ecf08b9651ba05f83f39cc10e73a11ac7
MaxRubel <maxrubel1@gmail.com>
2023-11-07 20:38:51 -0600

    Formatting to javascript worked great

--- main.js
+++ main.js
@@ -9,17 +9,52 @@ let user = {
   email: "github@tomJones.com",
   website: "https://www.tomjones.com",
   twitter: "@TommyJoJo",
-  highlights: `"Artict Code Vault Contributor <br>
-  Git Hub Star <br> PRO"`,
+  highlights: `Artict Code Vault Contributor <br>
+  Git Hub Star <br> PRO`,
   organizations: "",
   sponsors: "",
 };
 
-const userPhoto = document.querySelector("userPhoto");
-const realName = document.querySelector("realName");
-const userName = document.querySelector("userName");
-const tagLine = document.querySelector("tagLine");
-const stats = document.querySelector("stats");
-const personalInfo = document.querySelector("personalInfo");
-const highlights = document.querySelector("highlights");
-const sponsors = document.querySelector("sponsors");
+const sidebar = document.getElementById("sidebar");
+
+function renderSidebar() {
+  sidebar.innerHTML = `
+  <div id="sidebar">
+    <div class="userPhoto"></div>
+    <div class="realName">${user.name}</div>
+    <div class="userName">${user.userId}</div>
+    <div class="tagLine">
+${user.tagLine}
+    </div>
+    <div class="buttonRowSide">
+      <button type="button" class="btn btn-secondary">
+        Follow
+      </button>
+      <button type="button" class="btn btn-secondary">
+        Sponsor
+      </button>
+      <button type="button" class="btn btn-secondary">
+        ...
+      </button>
+    </div>
+    <div class="stats">${user.stats}</div>
+    <div class="personalInfo">
+      <ul>${user.location}</ul>
+      <ul>${user.email}</ul>
+      <ul>${user.website}</ul>
+      <ul>${user.twitter}</ul>
+    </div>
+    <div class="highlights">
+      <h1>Highlights</h1>
+${user.highlights}
+    </div>
+    <div class="organizations">
+      <h1>Organizations</h1>${user.organizations}
+    </div>
+    <div class="sponsors">
+      <h1>Sponsors</h1>${user.sponsors}
+    </div>
+  </div>`;
+}
+
+renderSidebar();
